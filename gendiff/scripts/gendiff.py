d8e6a8d13c603362860403fcbe2e324d3273689d
zwg <zwg.work.mail@gmail.com>
2023-02-15 12:42:18 +0300

    fixes

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,10 +2,46 @@
 import argparse
 from . import gendiff_yml
 from . import gendiff_json
-from . import gendata
+#from . import gendata
 from . import stylish_format
 from . import plain_format
 from . import json_format
+
+
+def generate_diff(first, second):
+    ss = set(list(first) + list(second))
+    data_list = {}
+    for i in dict.fromkeys(sorted(ss)):
+        diff = {}
+        if not (type(first.get(i)) == dict and type(second.get(i)) == dict):
+            if i in first and i in second:
+                if first.get(i) == second.get(i):
+                    diff["Key"] = i
+                    diff["Type"] = "same"
+                    diff["Value"] = second.get(i)
+                    data_list[i] = diff
+                else:
+                    diff["Key"] = i
+                    diff["Type"] = "changed"
+                    diff["Value"] = first.get(i)
+                    diff["Value_new"] = second.get(i)
+                    data_list[i] = diff
+            elif i not in second:
+                diff["Key"] = i
+                diff["Type"] = "removed"
+                diff["Value"] = first.get(i)
+                data_list[i] = diff
+            else:
+                diff["Key"] = i
+                diff["Type"] = "added"
+                diff["Value"] = second.get(i)
+                data_list[i] = diff
+        else:
+            diff["Key"] = i
+            diff["Type"] = "parent"
+            diff["Value"] = generate_diff(first[i], second[i])
+            data_list[i] = diff
+    return data_list
 
 
 def main():
