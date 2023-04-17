2678f33d2af754cf882fd917e1405f61ac83e3f6
zwg <zwg.work.mail@gmail.com>
2023-04-17 13:19:46 +0300

    minor refactoring

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -13,10 +13,6 @@ def generate_diff(filepath1, filepath2, format_type="stylish"):
         return (json_format.json_(
             diff_maker.make_diff(
                 filepath1, filepath2, format_type)))
-    elif format_type != "stylish_format" or "stylish":
-        raise Exception('Incorrect fomat type.'
-                        'The available formaters are:'
-                        ' "plain", "stylish" and "json".')
     else:
         return (stylish_format.stylish(
             diff_maker.make_diff(filepath1, filepath2, format_type)))
