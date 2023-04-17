37c02bba27e705672758cd741e7e8dab2829588e
zwg <zwg.work.mail@gmail.com>
2023-04-17 13:37:20 +0300

    added tests for coverage

---
+++ tests/test_gendiff_json.py
@@ -0,0 +1,49 @@
+import pytest
+import os
+from gendiff.formaters import stylish_format
+from gendiff.formaters import plain_format
+from gendiff import data_reader
+from gendiff import diff_maker
+
+
+@pytest.fixture
+def open_file_1():
+    return data_reader.file_opener(os.path.abspath(
+        'tests/fixtures/test_json/file3.json'))
+
+
+def test_open(open_file_1):
+    with open(os.path.abspath(
+            'tests/fixtures/test_json/diff_open.md')) as test_file:
+        test_file = test_file.read().rstrip('\n')
+    assert str(open_file_1) == test_file
+
+
+def test_gen_raw():
+    files = diff_maker.make_diff((
+        os.path.abspath('tests/fixtures/test_json/file3.json')),
+        (os.path.abspath('tests/fixtures/test_json/file4.json')))
+    with open(os.path.abspath(
+            'tests/fixtures/test_json/diff_result_raw.md')) as test_file:
+        test_file = test_file.read().rstrip('\n')
+    assert str(files) == test_file
+
+
+def test_stylish():
+    raw = diff_maker.make_diff((
+        os.path.abspath('tests/fixtures/test_json/file3.json')),
+        (os.path.abspath('tests/fixtures/test_json/file4.json')))
+    final_diff = stylish_format.stylish(raw)
+    with open('tests/fixtures/test_json/diff_result_stylish.md') as test_file:
+        test_file = test_file.read().rstrip('\n')
+    assert final_diff == test_file
+
+
+def test_plain():
+    raw = diff_maker.make_diff((
+        os.path.abspath('tests/fixtures/test_json/file3.json')),
+        (os.path.abspath('tests/fixtures/test_json/file4.json')))
+    final_diff = plain_format.plain(plain_format.same_deleter(raw))
+    with open('tests/fixtures/test_json/diff_result_plain.md') as test_file:
+        test_file = test_file.read().rstrip('\n')
+    assert final_diff == test_file
