b91ef5f336a7912c3a1c83f5f3c699b087d0ceff
zwg <zwg.work.mail@gmail.com>
2023-04-20 12:41:38 +0300

    minor changes for lint

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -26,7 +26,7 @@ def correct_raw():
 def correct_stylish():
     with open(
             'tests/fixtures/test_json/diff_result_stylish.md'
-             ) as correct_result:
+            ) as correct_result:
         correct_result = correct_result.read().rstrip('\n')
     return correct_result
 
@@ -73,10 +73,10 @@ def plain_output():
 
 @pytest.mark.parametrize("result, correct_result",
                          [
-                          ("open_file_output", "correct_open"),
-                          ("raw_diff_output", "correct_raw"),
-                          ("stylish_output", "correct_stylish"),
-                          ("plain_output", "correct_plain")
+                            ("open_file_output", "correct_open"),
+                            ("raw_diff_output", "correct_raw"),
+                            ("stylish_output", "correct_stylish"),
+                            ("plain_output", "correct_plain")
                             ])
 def test_all(result, correct_result, request):
     result = request.getfixturevalue(result)
