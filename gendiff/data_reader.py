87cf6000af2cd4131d3361972ef2ad9f22a5adc5
zwg <zwg.work.mail@gmail.com>
2023-04-22 12:06:58 +0300

    changes to data_reader minor refactoring

--- gendiff/data_reader.py
+++ gendiff/data_reader.py
@@ -5,12 +5,11 @@ import yaml
 def file_reader(filepath, opened_file):
     if str(filepath).endswith('.json'):
         file = json.load(opened_file)
-        return file
     elif str(filepath).endswith('.yml') or str(filepath).endswith('.yaml'):
         file = yaml.load(opened_file, Loader=yaml.FullLoader)
-        return file
     else:
         raise Exception("Please use .json or .yaml files only")
+    return file
 
 
 def file_opener(filepath):
